7ca362e7bf4ea5c328fbbf25fd3f0a1cd148e0d9
wkybama <35328194+wkinley@users.noreply.github.com>
2025-12-11 21:57:51 +0800

    Add files via upload

---
+++ src/main/java/com/jlwan/sonartest/sonartest/mvn/SonarTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jlwan.sonartest.sonartest.mvn;
+
+/**
+ *
+ * @author unm2000
+ */
+public class SonarTest {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String s = "a:b";
+        System.out.println(s.substring(0, s.lastIndexOf(":")));
+    }
+}
