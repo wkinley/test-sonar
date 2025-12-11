5e9513c045fa08d5d560c9b703b57c1383bc734f
wkybama <35328194+wkinley@users.noreply.github.com>
2025-12-11 22:05:16 +0800

    null

--- src/main/java/com/jlwan/sonartest/sonartest/mvn/SonarTest.java
+++ src/main/java/com/jlwan/sonartest/sonartest/mvn/SonarTest.java
@@ -5,16 +5,29 @@
  */
 package com.jlwan.sonartest.sonartest.mvn;
 
+import java.util.Random;
+
 /**
  *
  * @author unm2000
  */
 public class SonarTest {
-     /**
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String s = "a:b";
         System.out.println(s.substring(0, s.lastIndexOf(":")));
+
+        int b = getString(System.currentTimeMillis()).length();
+        System.out.println(b);
+    }
+
+    public static String getString(long k) {
+        if (k % 2 == 0) {
+            return null;
+        }
+        return k + "";
     }
 }
